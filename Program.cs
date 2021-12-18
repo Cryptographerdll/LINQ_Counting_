af7a543b4706a915534bbacb78668e1aa7935dea
Cryptographer <85350190+khalilDup@users.noreply.github.com>
2021-12-18 14:27:51 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace LINQ_Counting_Operations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[] { 1, 5, 2, 32, 4, 8 };
+
+            var count = numbers.Count();
+
+            Console.WriteLine(count);
+        }
+    }
+}
